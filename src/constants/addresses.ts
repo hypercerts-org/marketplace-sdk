f35c0c2909e7f080b67a15d902ee3acbdafe5678
jipstavenuiter <jipstavenuiter@gmail.com>
2024-01-16 19:30:42 +0100

    (fix): use correct chain id

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,9 +2,9 @@ import { Addresses, ChainId } from "../types";
 import { deployments } from "@hypercerts-org/sdk";
 
 const sepoliaAddresses: Addresses = {
-  EXCHANGE_V2: deployments[10].addresses?.HypercertExchange!,
-  TRANSFER_MANAGER_V2: deployments[10].addresses?.TransferManager!,
-  ORDER_VALIDATOR_V2: deployments[10].addresses?.OrderValidator!,
+  EXCHANGE_V2: deployments[11155111].addresses?.HypercertExchange!,
+  TRANSFER_MANAGER_V2: deployments[11155111].addresses?.TransferManager!,
+  ORDER_VALIDATOR_V2: deployments[11155111].addresses?.OrderValidator!,
   WETH: "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9",
 };
 
